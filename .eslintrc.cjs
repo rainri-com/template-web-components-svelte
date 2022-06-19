7527112c75f937555210b27d839f2de8ab553f02
Com Rainri <rainri.com@gmail.com>
2022-06-19 20:04:11 +0900

    eslint settings

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+    extraFileExtensions: [".svelte"],
+  },
+  plugins: ["svelte3", "@typescript-eslint"],
+  rules: {},
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
+    },
+  ],
+  settings: {
+    "svelte3/typescript": () => require("typescript"),
+  },
+};
